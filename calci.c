c126c305ac0e4cc0cfd9dd7c69ec539e2d901e0b
Nithya392 <nithyashree.b@s.amity.edu>
2025-04-16 15:56:55 +0530

    code to perform operations from choice choosen from switch case

--- calci.c
+++ calci.c
@@ -39,3 +39,21 @@ int main() {
 
     return 0;
 }
+   
+int add(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+float divide(int a, int b) {
+    return (float)a / b;
+}
+
+
