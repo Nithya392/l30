134617a5252b19ee7deb6d8891705e87225fc3a7
Nithya392 <nithyashree.b@s.amity.edu>
2025-04-16 15:49:34 +0530

    code for taking input from user to choose which operation to be performed

--- calci.c
+++ calci.c
@@ -5,3 +5,13 @@ int subtract(int a, int b);
 int multiply(int a, int b);
 float divide(int a, int b);
 
+int main() {
+    int choice, num1, num2;
+
+    printf("Simple Calculator \n");
+    printf("Choose 1-4 options to perform operations\n");
+    printf("1. Addition\n 2. Subtraction\n 3. Multiplication\n 4. Division\n");
+    printf("Enter your choice (1-4): ");
+    scanf("%d", &choice);
+
+   
