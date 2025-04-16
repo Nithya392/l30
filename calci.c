65e059ff5e638a598d24ee61e0724ce4356303ef
Nithya392 <nithyashree.b@s.amity.edu>
2025-04-16 15:53:04 +0530

    added switch case

--- calci.c
+++ calci.c
@@ -17,4 +17,25 @@ int main() {
     printf("Enter two numbers: ");
     scanf("%d %d", &num1, &num2);
     
+    switch (choice) {
+        case 1:
+            printf("Result: %d\n", add(num1, num2));
+            break;
+        case 2:
+            printf("Result: %d\n", subtract(num1, num2));
+            break;
+        case 3:
+            printf("Result: %d\n", multiply(num1, num2));
+            break;
+        case 4:
+            if (num2 == 0)
+                printf("Error! Division by zero is not allowed.\n");
+            else
+                printf("Result: %.2f\n", divide(num1, num2));
+            break;
+        default:
+            printf("Invalid choice!\n");
+    }
 
+    return 0;
+}
